c5d5904032fea1935c84f22c732505dca3805d98
abhinavv22 <96390949+abhinavv22@users.noreply.github.com>
2025-07-15 14:22:42 +0530

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -5,11 +5,12 @@ import { Astronaut } from "../components/Astronaut";
 import { Float } from "@react-three/drei";
 import { useMediaQuery } from "react-responsive";
 import { easing } from "maath";
-import { Suspense } from "react";
+import { Suspense, useRef } from "react";
 import Loader from "../components/Loader";
 
 const Hero = () => {
   const isMobile = useMediaQuery({ maxWidth: 853 });
+
   return (
     <section className="flex items-start justify-center min-h-screen overflow-hidden md:items-start md:justify-start c-space">
       <HeroText />
@@ -22,8 +23,8 @@ const Hero = () => {
           <Suspense fallback={<Loader />}>
             <Float>
               <Astronaut
-                scale={isMobile && 0.23}
-                position={isMobile && [0, -1.5, 0]}
+                scale={isMobile ? 0.23 : 0.4}
+                position={isMobile ? [0, -1.5, 0] : [0, -0.5, 0]}
               />
             </Float>
             <Rig />
@@ -35,14 +36,14 @@ const Hero = () => {
 };
 
 function Rig() {
-  return useFrame((state, delta) => {
-    easing.damp3(
-      state.camera.position,
-      [state.mouse.x / 10, 1 + state.mouse.y / 10, 3],
-      0.5,
-      delta
-    );
+  const ref = useRef();
+
+  useFrame((state, delta) => {
+    easing.damp3(state.camera.position, [0, 0, 5], 0.25, delta);
+    state.camera.lookAt(0, 0, 0);
   });
+
+  return null;
 }
 
 export default Hero;
